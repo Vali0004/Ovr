b6080cabd95d122a69f980b5816745f663346d8f
Forever Gone <78507032+Faint0002@users.noreply.github.com>
2023-06-23 04:22:28 -0400

    Added a check to m_host in updatePresenceAttributeInt

--- Ovr/src/hooking/hooks/update_presence_attribute_int.cpp
+++ Ovr/src/hooking/hooks/update_presence_attribute_int.cpp
@@ -13,8 +13,13 @@ bool hooks::updateAttributeInt(PresenceData* Data, int ProfileIndex, char* Attri
 		std::string playerCount{ "no" };
 		if (g_statistics.m_playerCount)
 			playerCount = std::to_string(g_statistics.m_playerCount);
-		std::string host{ g_statistics.m_host->GetName() };
-		LOG(Session, "Joined a new {} session with {} player{} hosted by {}", g_statistics.m_gameType, g_statistics.m_playerCount == 1 ? "" : "s", playerCount, host);
+		if (g_statistics.m_host && g_statistics.m_host->IsConnected()) {
+			std::string host{ g_statistics.m_host->GetName() };
+			LOG(Session, "Joined a new {} session with {} player{} hosted by {}", g_statistics.m_gameType, g_statistics.m_playerCount == 1 ? "" : "s", playerCount, host);
+		}
+		else {
+			LOG(Session, "Joined a new {} session with {} player{}", g_statistics.m_gameType, g_statistics.m_playerCount == 1 ? "" : "s", playerCount);
+		}
 	} break;
 	}
 	RET_CALL_DECL(updateAttributeInt, Data, ProfileIndex, Attribute, Value);
